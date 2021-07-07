1ea9e5707eb6baed29084d31998ca29675358b31
mariusmoldovan <biris_21@yahoo.com>
2021-07-07 10:18:34 -0500

    get by id added

--- api/server.js
+++ api/server.js
@@ -21,6 +21,22 @@ server.get('/api/users', (req,res) => {
 })
 
 
+server.get('/api/users/:id', (req,res) => {
+    User.findById(req.params.id)
+    .then(user => {
+        res.json(user)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'error getting user',
+            err: err.message,
+            stack: err.stack
+        })
+    })
+})
+
+
+
 server.use('*', (req,res) => {
     res.status(404).json({
         message: 'not found'
