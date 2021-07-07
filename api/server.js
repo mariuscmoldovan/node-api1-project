97428524f60a1852e9c9a5a5265439892948d99f
mariusmoldovan <biris_21@yahoo.com>
2021-07-07 10:06:56 -0500

    get method added

--- api/server.js
+++ api/server.js
@@ -2,7 +2,25 @@
 
 const express = require('express')
 
+const User = require('./users/model')
+
 const server = express()
+
+server.get('/api/users', (req,res) => {
+    User.find()
+    .then(users => {
+       res.json(users)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'error getting users',
+            err: err.message,
+            stack: err.stack
+        })
+    })
+})
+
+
 server.use('*', (req,res) => {
     res.status(404).json({
         message: 'not found'
