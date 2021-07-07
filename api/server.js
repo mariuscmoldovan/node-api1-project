c0e77ba35818bf5cd73b275ec14d269bcec04021
mariusmoldovan <biris_21@yahoo.com>
2021-07-07 17:44:20 -0500

    delete add

--- api/server.js
+++ api/server.js
@@ -63,6 +63,24 @@ server.post('/api/users', (req, res) => {
         })
     }
 })
+server.delete('/api/users/:id',  async (req, res) => {
+   try { const pisibleUser = await User.findById(req.params.id)
+    if (!pisibleUser) {
+        res.status(404).json({
+            message: "The user with the specified ID does not exist",
+        })
+    }else {
+        const deletedUser = await User.remove(pisibleUser.id)
+        res.status(200).json(deletedUser)
+    }
+} catch(err) {
+    res.status(500).json({
+        message: "There was an error while saving the user to the database" ,
+        err: err.message,
+        stack: err.stack
+    })
+}
+})
 
 
 server.use('*', (req,res) => {
